6ac8678ebbcea6b572a4bcb7d7b9c4c21baff692
Mehul Velani <velanimehul11@gmail.com>
2022-03-11 14:56:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
  * @var bool
  */
 define('WP_USE_THEMES', true);
-echo "asd"; 
+echo "asd";
+echo "2"; 
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
